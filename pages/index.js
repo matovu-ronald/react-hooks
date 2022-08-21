c3df78554b0f6052c6f08cad23c9a3a262194890
Matovu Ronald <maronwhite6@gmail.com>
2022-08-21 19:53:00 +0300

    Reusing form logic with custom hooks

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,29 @@
 import { Fragment, useState } from "react";
+import { useInput } from "../hooks/useInput";
 
 export default function Home() {
-  const [sound, setSound] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [soundProps, resetSoundProps] = useInput("");
+  const [colorProps, resetColorProps] = useInput("#000000");
 
   const submit = (e) => {
     e.preventDefault();
 
-    alert(`${sound} sounds like ${color}`);
-    setSound("");
-    setColor("");
+    alert(`${soundProps.value} sounds like ${colorProps.value}`);
+    resetColorProps();
+    resetSoundProps();
   };
 
   return (
     <Fragment>
       <form onSubmit={submit}>
         <input
-          value={sound}
-          onChange={(e) => setSound(e.target.value)}
+          {...soundProps}
           type="text"
           name="sound"
           id="sound"
           placeholder="Sound..."
         />
-        <input
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-          type="color"
-          name="color"
-          id="color"
-        />
+        <input {...colorProps} type="color" name="color" id="color" />
         <button type="submit">ADD</button>
       </form>
     </Fragment>
