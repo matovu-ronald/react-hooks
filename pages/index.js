4f4a7fc055b059a8a158336c0c03db456237257f
Matovu Ronald <maronwhite6@gmail.com>
2022-08-21 16:10:05 +0300

    Dispatching actions with useReducer

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
 import { Fragment, useEffect, useReducer, useState } from "react";
 
+const initialState = {
+  message: "hi",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "yell":
+      return {
+        message: `HEY! I JUST SAID ${state.message}`,
+      };
+
+    case "whisper":
+      return {
+        message: `excuse me I JUST SAID ${state.message}`,
+      };
+  }
+}
+
 export default function Home() {
-  const [checked, toggle] = useReducer((checked) => !checked, false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <Fragment>
-      <input
-        type="checkbox"
-        name="status"
-        id="status"
-        value={checked}
-        onChange={toggle}
-      />
-      <p>{checked ? "Delivered" : "Not Delivered"}</p>
+      <p>Message: {state.message}</p>
+      <button onClick={() => dispatch({ type: "yell" })}>YELL</button>
+      <button onClick={() => dispatch({ type: "whisper" })}>Whisper</button>
     </Fragment>
   );
 }
